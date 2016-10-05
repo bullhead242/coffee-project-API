5d1bf0dfa82edbfda2491d0933ca6796404b4882
bullhead242 <bullhead242@gmail.com>
2016-10-05 03:19:39 -0400

    change to client origins in config/application.rb to match gh-pages deployment

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module CoffeeProjectApi
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_ORIGIN'] || 'http://localhost:8080'
+        origins ENV['CLIENT_ORIGIN'] || 'https://bullhead242.github.io/coffee-project-frontend/'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
